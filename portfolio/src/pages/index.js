4cde472fc4fbae6f1de372f1134d9cce7b00d19d
Eduardo S. Libardi <eslibardi@gmail.com>
2021-12-11 20:27:39 -0300

    projects component

--- portfolio/src/pages/index.js
+++ portfolio/src/pages/index.js
@@ -5,16 +5,47 @@ import Services from "../components/Services"
 import Jobs from "../components/Jobs"
 import Projects from "../components/Projects"
 import Seo from "../components/Seo"
-const IndexPage = () => {
+const IndexPage = ({ data }) => {
+  const {
+    allStrapiProject: { nodes: projects },
+  } = data
   return (
     <>
       <main>
         <Hero />
         <Services />
         <Jobs />
+        <Projects title="featured projects" showLink projects={projects} />
       </main>
     </>
   )
 }
+
+export const query = graphql`
+  {
+    allStrapiProject(filter: { featured: { eq: true } }) {
+      nodes {
+        description
+        featured
+        github
+        slug
+        id
+        title
+        url
+        stack {
+          title
+          id
+        }
+        image {
+          localFile {
+            childImageSharp {
+              gatsbyImageData(layout: CONSTRAINED, placeholder: BLURRED)
+            }
+          }
+        }
+      }
+    }
+  }
+`
 
 export default IndexPage
