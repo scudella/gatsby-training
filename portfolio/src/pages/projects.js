577a4091834628b7da7074b2d54c103b90768944
Eduardo S. Libardi <eslibardi@gmail.com>
2021-12-12 20:43:12 -0300

    projects page

--- portfolio/src/pages/projects.js
+++ portfolio/src/pages/projects.js
@@ -2,12 +2,47 @@ import React from "react"
 import { graphql } from "gatsby"
 import Projects from "../components/Projects"
 import Seo from "../components/Seo"
-const ProjectsPage = () => {
+const ProjectsPage = ({
+  data: {
+    allStrapiProject: { nodes: projects },
+  },
+}) => {
   return (
     <>
-      <h2>projects page</h2>
+      <main>
+        <section className="projects-page">
+          <Projects title="all projects" projects={projects} />
+        </section>
+      </main>
     </>
   )
 }
+
+export const query = graphql`
+  {
+    allStrapiProject {
+      nodes {
+        description
+        featured
+        github
+        slug
+        id
+        title
+        url
+        stack {
+          title
+          id
+        }
+        image {
+          localFile {
+            childImageSharp {
+              gatsbyImageData(layout: CONSTRAINED, placeholder: BLURRED)
+            }
+          }
+        }
+      }
+    }
+  }
+`
 
 export default ProjectsPage
