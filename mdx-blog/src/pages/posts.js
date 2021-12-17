a47b4c3032311be8431cd783f6732e25dc857f81
Eduardo S. Libardi <eslibardi@gmail.com>
2021-12-17 18:46:40 -0300

    posts page

--- mdx-blog/src/pages/posts.js
+++ mdx-blog/src/pages/posts.js
@@ -3,10 +3,41 @@ import Layout from '../components/Layout'
 import Hero from '../components/Hero'
 import { graphql } from 'gatsby'
 import Posts from '../components/Posts'
-const PostsPage = () => {
-return <h4>posts page</h4>
+const PostsPage = ({ data }) => {
+  const {
+    allMdx: { nodes: posts },
+  } = data
+  console.log(posts)
+  return (
+    <Layout>
+      <Hero />
+      <Posts posts={posts} title="all posts" />
+    </Layout>
+  )
 }
 
-
+export const query = graphql`
+  {
+    allMdx(sort: { fields: frontmatter___date, order: DESC }) {
+      nodes {
+        id
+        excerpt
+        frontmatter {
+          slug
+          date(formatString: "MMMM, Do YYYY")
+          author
+          category
+          readTime
+          image {
+            childImageSharp {
+              gatsbyImageData
+            }
+          }
+          title
+        }
+      }
+    }
+  }
+`
 
 export default PostsPage
