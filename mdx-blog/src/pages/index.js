29c8d9ba3f1815d211b90df43ac69abf0e75eae7
Eduardo S. Libardi <eslibardi@gmail.com>
2021-12-17 11:24:56 -0300

    setup graphql for posts
    include Posts component at home
    move first post to posts and frontmatter it

--- mdx-blog/src/pages/index.js
+++ mdx-blog/src/pages/index.js
@@ -3,12 +3,39 @@ import Hero from '../components/Hero'
 import Layout from '../components/Layout'
 import Posts from '../components/Posts'
 import { graphql } from 'gatsby'
-const IndexPage = () => {
+const IndexPage = ({ data }) => {
+  const {
+    allMdx: { nodes: posts },
+  } = data
   return (
     <Layout>
       <Hero showPerson />
+      <Posts posts={posts} title="recently published" />
     </Layout>
   )
 }
+
+export const query = graphql`
+  {
+    allMdx(limit: 3, sort: { fields: frontmatter___date, order: DESC }) {
+      nodes {
+        id
+        excerpt
+        frontmatter {
+          slug
+          date(formatString: "MMMM, Do YYYY")
+          author
+          category
+          readTime
+          image {
+            childImageSharp {
+              gatsbyImageData
+            }
+          }
+        }
+      }
+    }
+  }
+`
 
 export default IndexPage
