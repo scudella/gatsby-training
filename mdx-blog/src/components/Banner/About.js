470f10456cb5bf0d8bdbdd7150f6fe6b9baf3743
Eduardo S. Libardi <eslibardi@gmail.com>
2021-12-20 14:07:36 -0300

    Banner components

--- mdx-blog/src/components/Banner/About.js
+++ mdx-blog/src/components/Banner/About.js
@@ -7,7 +7,20 @@ import styled from 'styled-components'
 const About = () => {
   return (
     <Wrapper>
-      Banner About
+      <Title title="about me" />
+      <StaticImage
+        src="../../assets/banner-about.jpeg"
+        layout="fixed"
+        width={100}
+        height={100}
+        alt="author"
+        className="img"
+      />
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquam, ad
+        quasi? Voluptates excepturi consequuntur alias?
+      </p>
+      <SocialLinks styleClass="banner-icons" />
     </Wrapper>
   )
 }
