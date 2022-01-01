6f6cd5e243a8d18cc27149551056a4aea797b068
Eduardo S. Libardi <eslibardi@gmail.com>
2022-01-01 18:27:51 -0300

    fix warning on variable not used and some whitespaces

--- design/src/constants/algolia.js
+++ design/src/constants/algolia.js
@@ -1,23 +1,23 @@
 const airtableQuery = `
-   {
-    allAirtable(filter: {table: {eq: "Projects"}}) {
-      nodes {
-        id
-        data {
-          date
-          name
-          type
-          image {
-            localFiles {
-              childImageSharp {
-                gatsbyImageData
-              }
+{
+allAirtable(filter: {table: {eq: "Projects"}}) {
+  nodes {
+      id
+      data {
+        date
+        name
+        type
+        image {
+          localFiles {
+            childImageSharp {
+              gatsbyImageData
             }
           }
         }
       }
     }
   }
+}
 `
 
 function pageToAlgoliaRecord({ id, data: { name, type, date, image } }) {
