bd72b1b96aee390cb076c0ed53cd9d2534f03c7b
Eduardo S. Libardi <eslibardi@gmail.com>
2022-01-01 20:45:12 -0300

    export Seo from components index.js

--- design/src/components/index.js
+++ design/src/components/index.js
@@ -1,9 +1,20 @@
-import Layout from "./Layout"
-import Hero from "./Hero"
-import About from "./About"
-import Projects from "./Projects"
-import Survey from "./Survey"
-import Slider from "./Slider"
-import Algolia from "./Algolia"
-import GridProjects from "./GridProjects"
-export { Layout, Hero, About, Projects, Survey, Slider, Algolia, GridProjects }
+import Layout from './Layout'
+import Hero from './Hero'
+import About from './About'
+import Projects from './Projects'
+import Survey from './Survey'
+import Slider from './Slider'
+import Algolia from './Algolia'
+import GridProjects from './GridProjects'
+import Seo from './SEO.js'
+export {
+  Layout,
+  Hero,
+  About,
+  Projects,
+  Survey,
+  Slider,
+  Algolia,
+  GridProjects,
+  Seo,
+}
